9fc95bac83a2870473474c27337c1c96513de751
Lenovo <Lenovo@Bibhudatta>
2022-03-25 14:09:54 +0530

    java docs are added

--- src/test/java/com/crm/padma/NewDemo.java
+++ src/test/java/com/crm/padma/NewDemo.java
@@ -1,9 +1,17 @@
 package com.crm.padma;
 
 import org.testng.annotations.Test;
-
+/**
+ * 
+ * 
+ * @author Lenovo
+ *
+ */
 public class NewDemo {
-	
+	/**
+	 * 
+	 * 
+	 */
 	@Test
 	public void gm() {
 		System.out.println("good mrng");
@@ -16,6 +24,7 @@ public class NewDemo {
 		System.out.println("its afternoon");
 		
 		System.out.println("++++++++++++++");
+		System.out.println("Java docs are added");
 
 	}
 
